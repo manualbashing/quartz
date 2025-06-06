bbaffa99e6cb11a9f801304605462e7b68ca2fed
Manuel Batsching <m.batsching@abtis.de>
2025-06-06 12:46:31 +0000

    feat: :art: customize layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,66 +3,70 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+    head: Component.Head(),
+    header: [],
+    afterBody: [
+        Component.ConditionalRender({
+            component: Component.RecentNotes({ title: "" }),
+            condition: (page) => page.fileData.slug == "index",
+        }),
+    ],
+    footer: Component.Footer({
+        links: {
+            RSS: "https://manuel.batsching.cloud/index.xml",
+            Mastodon: "https://nerdculture.de/@mba",
+            GitHub: "https://github.com/manualbashing",
+        },
+    }),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+    beforeBody: [
+        Component.Flex({
+            components: [
+                { Component: Component.Search() },
+                {
+                    Component: Component.Breadcrumbs({
+                        rootName: "🏡",
+                    }),
+                },
+            ],
+        }),
+        Component.Flex({
+            components: [
+                { Component: Component.ArticleTitle() },
+                { Component: Component.Darkmode() },
+            ]
+        }),
+        Component.ContentMeta({ showReadingTime: false }),
+        Component.TableOfContents({ layout: "legacy" }),
+    ],
+    left: [],
+    right: []
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [],
+    beforeBody: [
+        Component.Flex({
+            components: [
+                { Component: Component.Search() },
+                {
+                    Component: Component.Breadcrumbs({
+                        rootName: "🏡",
+                    }),
+                },
+            ],
+        }),
+        Component.Flex({
+            components: [
+                { Component: Component.ArticleTitle() },
+                { Component: Component.Darkmode() },
+            ]
+        }),
+        Component.ContentMeta({ showReadingTime: false }),
+    ],
+    left: [],
+    right: [],
 }
